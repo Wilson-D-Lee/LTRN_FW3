0949dd08c700ac7d4d37a808ae9897501e33852f
Wilson Lee <wilson@lantern.ai>
2024-01-09 17:50:50 +0000

    Delete

--- Users/users.js
+++
@@ -1,9 +0,0 @@
-// users.js
-
-module.exports = {
-    basicUser: 'wilson@lantern.ai',
-    adminUser: 'wilson+admin@lantern.ai',
-    companyUser: 'wilson+company@lantern.ai',
-    sec2pword: process.env.USER_PASSWORD, // Password from environment variable
-    authCode: process.env.USER_AUTHCODE
-};
